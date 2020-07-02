bb6cd3ad5e48d845fe5ca7944227ae8b996a6727
sampletext32 <sampletext32@bk.ru>
2020-07-02 18:53:29 +0300

    Add project

---
+++ Goldov_2/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	char line[100];
+	cin.getline(line, 100);
+
+	int length = strlen(line);
+
+	int collect = 0;
+	for (int i = 0; i < length; i++)
+	{
+		if (isdigit(line[i]))
+		{
+			collect += line[i] - '0';
+		}
+	}
+	if (collect == length)
+	{
+		cout << "Длина строки равна сумме цифровых значений\n";
+	}
+	else
+	{
+		cout << "Длина строки не равна сумме цифровых значений\n";
+	}
+
+	system("pause");
+}
